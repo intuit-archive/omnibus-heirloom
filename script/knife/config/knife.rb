6ce87f6cdb74905fb0437b7b0d9629111bbf581b
Brett Weaver <brett@weav.net>
2013-02-05 13:16:33 -0800

    cleaned up duplicate settings

--- script/knife/config/knife.rb
+++ script/knife/config/knife.rb
@@ -1,10 +1,9 @@
 knife[:aws_access_key_id]     = "#{ENV['AWS_ACCESS_KEY_ID']}"
 knife[:aws_secret_access_key] = "#{ENV['AWS_SECRET_ACCESS_KEY']}"
-knife[:aws_ssh_key_id]        = 'control'
-knife[:chef_mode]             = 'solo'
-knife[:region]                = 'us-west-1'
-knife[:image]                 = 'ami-7b6a4e3e'
-knife[:flavor]                = 'c1.xlarge'
-knife[:ssh_user]              = 'ec2-user'
 knife[:aws_ssh_key_id]        = 'jenkins'    
+knife[:chef_mode]             = 'solo'
+knife[:flavor]                = 'c1.xlarge'
 knife[:identify_file]         = '~/.ssh/id_rsa'
+knife[:image]                 = 'ami-7b6a4e3e'
+knife[:region]                = 'us-west-1'
+knife[:ssh_user]              = 'ec2-user'
