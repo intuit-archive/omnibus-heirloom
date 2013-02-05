cf460cbfce0bf919332627fae596050c42479f8a
Brett Weaver <brett@weav.net>
2013-02-05 09:52:55 -0800

    adding knife config files

---
+++ script/knife/config/knife.rb
@@ -0,0 +1,10 @@
+knife[:aws_access_key_id]     = "#{ENV['AWS_ACCESS_KEY_ID']}"
+knife[:aws_secret_access_key] = "#{ENV['AWS_SECRET_ACCESS_KEY']}"
+knife[:aws_ssh_key_id]        = 'control'
+knife[:chef_mode]             = 'solo'
+knife[:region]                = 'us-west-1'
+knife[:image]                 = 'ami-7b6a4e3e'
+knife[:flavor]                = 'c1.xlarge'
+knife[:ssh_user]              = 'ec2-user'
+knife[:aws_ssh_key_id]        = 'jenkins'    
+knife[:identify_file]         = '~/.ssh/id_rsa'
