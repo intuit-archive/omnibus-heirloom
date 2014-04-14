e7e3ab34cb0bb1db379e386ad407bde37f11f517
Chad Cloes <chad_cloes@intuit.com>
2014-04-14 14:53:54 -0700

    Uhh... i guess I stutter in code now

--- config/software/heirloom.rb
+++ config/software/heirloom.rb
@@ -6,5 +6,5 @@ dependencies ["libxslt", "libxml2", "rubygems", "ruby"]
 env = { "GEM_HOME" => nil, "GEM_PATH" => nil }
 
 build do
-  gem "install heirloom -n #{install_dir}/bin --no-rdoc --no-ri -v #{version} --without jenkins --without jenkins", :env => env
+  gem "install heirloom -n #{install_dir}/bin --no-rdoc --no-ri -v #{version} --without jenkins", :env => env
 end
