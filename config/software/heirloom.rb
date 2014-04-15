ff7ee1d15e232d8f26e4dee196181b88f9ea10b0
Chad Cloes <chad_cloes@intuit.com>
2014-04-14 17:54:26 -0700

    Updated to include nokogiri as a dependency

--- config/software/heirloom.rb
+++ config/software/heirloom.rb
@@ -1,10 +1,10 @@
 name "heirloom"
 version ENV['version']
 
-dependencies ["libxslt", "libxml2", "rubygems", "ruby"]
+dependencies ["nokogiri", "rubygems", "ruby"]
 
 env = { "GEM_HOME" => nil, "GEM_PATH" => nil }
 
 build do
-  gem "install heirloom -n #{install_dir}/bin --no-rdoc --no-ri -v #{version} --without jenkins", :env => env
+  gem "install heirloom -n #{install_dir}/bin --no-rdoc --no-ri -v #{version}", :env => env
 end
